72cb213b6cc7c35fe9d07ec86a961affb151727d
remigerme <remi.germe@gmail.com>
2022-11-29 22:52:25 +0100

    New implementation of wildfire

--- src/wildfire.rs
+++ src/wildfire.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use crate::automaton::{Automaton, Access, Init, Rules};
+use crate::automaton::Automaton;
 
 
 const ROCK: u32 = 0;
@@ -18,98 +18,55 @@ const FADING_FIRE: u32 = 6;
 const RED: u32 = 0xBA2E0B;
 
 
-pub struct WildfireAutomaton(Automaton);
-
-
-impl WildfireAutomaton {
-    pub fn new(m: usize, n: usize, torus: bool, cells: Vec<Vec<u32>>) -> Self {
-        let q = 7;
-        let colors = vec![GREY, BLACK, GREEN, DARK_GREEN, YELLOW, ORANGE, RED];
-        WildfireAutomaton(Automaton::new(m, n, q, torus, colors, cells))
-    }
-}
-
-
-impl Rules for WildfireAutomaton {
-    fn next_state(&self, i: usize, j: usize) -> u32 {
-        let mut rng = rand::thread_rng();
-        match self.0.cells[i][j] {
-            ROCK => ROCK,
-            ASHES => if rng.gen::<f64>() < 0.001 {YOUNG} else {ASHES},
-            YOUNG => {
-                let neighbours = self.0.get_neighbours(i, j);
-                if rng.gen::<f64>() < 0.01 && neighbours.contains(&NEW_FIRE) {
-                    NEW_FIRE
-                } else if rng.gen::<f64>() < 0.02 && neighbours.contains(&FIRE) {
-                    NEW_FIRE
-                } else if rng.gen::<f64>() < 0.01 && neighbours.contains(&FADING_FIRE) {
-                    NEW_FIRE
-                } else if rng.gen::<f64>() < 0.005 {
-                    OLD
-                } else {
-                    YOUNG
-                }
-            },
-            OLD => {
-                let neighbours = self.0.get_neighbours(i, j);
-                if rng.gen::<f64>() < 0.1 && neighbours.contains(&NEW_FIRE) {
-                    NEW_FIRE
-                } else if rng.gen::<f64>() < 0.2 && neighbours.contains(&FIRE) {
-                    NEW_FIRE
-                } else if rng.gen::<f64>() < 0.1 && neighbours.contains(&FADING_FIRE) {
-                    NEW_FIRE
-                } else if rng.gen::<f64>() < 0.00005 && neighbours.iter().filter(|&n| *n == OLD).count() >= 5 {
-                    NEW_FIRE
-                } else {
-                    OLD
-                }
-            },
-            NEW_FIRE => if rng.gen::<f64>() < 0.1 {FIRE} else {NEW_FIRE},
-            FIRE => if rng.gen::<f64>() < 0.1 {FADING_FIRE} else {FIRE},
-            FADING_FIRE => if rng.gen::<f64>() < 0.1 {ASHES} else {FADING_FIRE},
-            _ => ROCK,
-        }
-    }
-
-    fn next(&mut self) {
-        let (i_min, i_max) = if self.0.torus {(0, self.0.m)} else {(1, self.0.m - 1)};
-        let (j_min, j_max) = if self.0.torus {(0, self.0.n)} else {(1, self.0.n - 1)};
-        for i in i_min..i_max {
-            for j in j_min..j_max {
-                self.0.temp[i][j] = self.next_state(i, j);
+fn next_state(a: &Automaton, i: usize, j: usize) -> u32 {
+    let mut rng = rand::thread_rng();
+    match a.get_cell(i, j) {
+        ROCK => ROCK,
+        ASHES => if rng.gen::<f64>() < 0.001 {YOUNG} else {ASHES},
+        YOUNG => {
+            let neighbours = a.get_neighbours(i, j);
+            if rng.gen::<f64>() < 0.01 && neighbours.contains(&NEW_FIRE) {
+                NEW_FIRE
+            } else if rng.gen::<f64>() < 0.02 && neighbours.contains(&FIRE) {
+                NEW_FIRE
+            } else if rng.gen::<f64>() < 0.01 && neighbours.contains(&FADING_FIRE) {
+                NEW_FIRE
+            } else if rng.gen::<f64>() < 0.005 {
+                OLD
+            } else {
+                YOUNG
             }
-        }
-        self.0.swap_buffer();
+        },
+        OLD => {
+            let neighbours = a.get_neighbours(i, j);
+            if rng.gen::<f64>() < 0.1 && neighbours.contains(&NEW_FIRE) {
+                NEW_FIRE
+            } else if rng.gen::<f64>() < 0.2 && neighbours.contains(&FIRE) {
+                NEW_FIRE
+            } else if rng.gen::<f64>() < 0.1 && neighbours.contains(&FADING_FIRE) {
+                NEW_FIRE
+            } else if rng.gen::<f64>() < 0.00005 && neighbours.iter().filter(|&n| *n == OLD).count() >= 5 {
+                NEW_FIRE
+            } else {
+                OLD
+            }
+        },
+        NEW_FIRE => if rng.gen::<f64>() < 0.1 {FIRE} else {NEW_FIRE},
+        FIRE => if rng.gen::<f64>() < 0.1 {FADING_FIRE} else {FIRE},
+        FADING_FIRE => if rng.gen::<f64>() < 0.1 {ASHES} else {FADING_FIRE},
+        _ => ROCK,
     }
 }
 
 
-// The end of the file is used to delegate functions
-impl Access for WildfireAutomaton {
-    fn get_size(&self) -> (usize, usize) {
-        self.0.get_size()
-    }
-
-    fn get_cells(&self) -> Vec<Vec<u32>> {
-        self.0.get_cells()
-    }
-
-    fn get_cell(&self, i: usize, j: usize) -> u32 {
-        self.0.get_cell(i, j)
-    }
-
-    fn get_cell_color(&self, i: usize, j: usize) -> u32 {
-        self.0.get_cell_color(i, j)
-    }
-}
-
-
-impl Init for WildfireAutomaton {
-    fn init_rand(&mut self) {
-        self.0.init_rand()
-    }
-
-    fn init_state(&mut self, s: u32) {
-        self.0.init_state(s)
-    }
+pub fn new(m: usize, n: usize, torus: bool) -> Automaton {
+    Automaton::new(
+        m,
+        n,
+        7,
+        torus,
+        Box::new(next_state),
+        vec![GREY, BLACK, GREEN, DARK_GREEN, YELLOW, ORANGE, RED],
+        vec![vec![ROCK; n]; m] 
+    )
 }
