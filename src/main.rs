d41e579bcb1f26c84720c22634115089960c4a6f
remigerme <remi.germe@gmail.com>
2022-12-06 14:52:40 +0100

    Declaring mods

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,9 @@
-use std::vec::Vec;
-use automaton_1d::{generate_image};
-use minifb::{Key, Window, WindowOptions};
-
-
 mod automaton_1d;
 mod automaton_2d;
-mod elementary;
-mod game_of_life;
-mod color_gradient;
-mod wildfire;
-mod epidemic;
+mod color;
 
+use automaton_1d::{generate_image, elementary};
+use minifb::{Key, Window, WindowOptions};
 
 fn main() {
     let m = 20000;
@@ -21,7 +14,7 @@ fn main() {
 
     let mut a = elementary::new(n, &22, false);
     let img = generate_image(width, height, cell_size, &mut a);
-    img.save("rule_22_4.png").unwrap();
+    img.save("rule_22.png").unwrap();
     // let mut buffer: Vec<u32> = vec![0; width * height];
     // 
     // let mut window = Window::new(
@@ -34,7 +27,7 @@ fn main() {
     //     panic!("{}", e);
     // });
     // window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
-// 
+    //
     // let mut automaton = elementary::new(n, &90, false);
     // let mut k = 0;
     // while window.is_open() && !window.is_key_down(Key::Escape) {
